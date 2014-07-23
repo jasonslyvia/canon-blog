4a9f812ed8d4412590fa92204ad205b964213fea
jasonslyvia <jasonslyvia@gmail.com>
2014-07-23 16:34:51 +0800

    init project

---
+++ index.php
@@ -0,0 +1,110 @@
+
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head profile="http://gmpg.org/xfn/11">
+<meta charset="utf-8" />
+<title>摄影圈博客</title>
+<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css" type="text/css" media="screen" />
+<style type="text/css" media="screen">
+  #page { background: url("http://www.aliued.com/wp-content/themes/aliued-theme-1.7/images/kubrickbgwide.jpg") repeat-y top; border: none; }
+</style>
+</head>
+<body class="aliued">
+  <div class="layout-wrap">
+    <!-- header -->
+    <div class="layout-header content-wrap clearfix">
+      <h1 class="header-logo">
+        <a href="http://www.aliued.com/">Alibaba UED—阿里巴巴国际UED团队</a>
+      </h1>   
+      <ul class="header-navi clearfix">
+        <li class="current_page_item"> <a href="http://www.aliued.com">Home</a> </li>
+        <li class="page_item page-item-7"><a href="http://www.aliued.com/about-us/">About us</a></li>
+        <li class="page_item page-item-8 page_item_has_children"><a href="http://www.aliued.com/join-us/">Join Us</a></li>
+      </ul>
+    </div>
+
+    <!-- content -->
+    <div class="layout-content content-wrap clearfix">
+      <div id="content" class="layout-main">
+
+        <div class="entry" id="post-2480">
+          <div class="entry-meta">
+            <span class="entry-author-gravatar">
+            <img alt='' src='http://0.gravatar.com/avatar/8827ed9a7be1b641b4c2b3887824951c?s=55&amp;d=http%3A%2F%2F0.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D55&amp;r=G' class='avatar avatar-55 photo' height='55' width='55' />          </span>
+            
+            <h2 class="entry-title">
+              <a href="http://www.aliued.com/2014/07/22/2480/" rel="bookmark" title="Permanent Link to 水到渠成 &#8211; 用户体验架构的探索和实践（附PDF）">水到渠成 &#8211; 用户体验架构的探索和实践（附PDF）</a>
+            </h2>
+            
+            <span class="entry-author">Ryan</span>
+            <span class="entry-category"><a href="http://www.aliued.com/category/interaction-design/" title="查看交互设计中的全部文章" rel="category tag">交互设计</a>, <a href="http://www.aliued.com/category/life/" title="查看团队生活中的全部文章" rel="category tag">团队生活</a>, <a href="http://www.aliued.com/category/share/" title="查看知识分享中的全部文章" rel="category tag">知识分享</a></span>
+            <span class="entry-date">2014/07/22</span>
+            <span class="entry_comment"><a href="http://www.aliued.com/2014/07/22/2480/#comments" title="《水到渠成 &#8211; 用户体验架构的探索和实践（附PDF）》上的评论">2</a>         </span>
+          </div>
+          <div class="entry-content">
+            <div style="color: #666666;">
+              <p><a href="http://www.aliued.com/2014/07/22/2480/"><img class="alignnone" alt="" src="http://www.aliued.com/wp-content/uploads/2014/07/ixdc.jpg" width="730" height="250" /></a></p>
+              <p><a href="http://www.aliued.com/2014/07/22/2480/#more-2480" class="more-link">Read the rest of this entry &raquo;</a></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="page-navi clearfix">
+          <span class="next"><a href="http://www.aliued.com/page/2/" >更旧的文章 &raquo;</a></span>
+          <span class="previous"></span>
+        </div>
+      </div>
+      
+      <!-- sidebar -->
+      <div class="layout-sub">
+        <ul>
+          <li id="search-3" class="widget widget_search"><form role="search" method="get" id="searchform" class="searchform" action="http://www.aliued.com/">
+              <div>
+                <label class="screen-reader-text" for="s">搜索：</label>
+                <input type="text" value="" name="s" id="s" />
+                <input type="submit" id="searchsubmit" value="搜索" />
+              </div>
+            </form>
+          </li>
+          <li id="text-4" class="widget widget_text">
+            <h2 class="widgettitle">微博</h2>
+            <div class="textwidget">
+              <iframe width="100%" height="400" class="share_self"  frameborder="0" scrolling="no" src="http://widget.weibo.com/weiboshow/index.php?language=&width=0&height=400&fansRow=2&ptype=1&speed=0&skin=2&isTitle=0&noborder=0&isWeibo=1&isFans=0&uid=2855231312&verifier=9d7e9f69&dpc=1"></iframe></div>
+          </li>
+          <li id="categories-410708512" class="widget widget_categories">
+            <h2 class="widgettitle">文章分类</h2>
+            <ul>
+              <li class="cat-item cat-item-12"> <a href="http://www.aliued.com/category/interaction-design/" title="查看交互设计下的所有文章">交互设计</a> </li>
+              <li class="cat-item cat-item-14"><a href="http://www.aliued.com/category/web-developer/" title="查看前端开发下的所有文章">前端开发</a> </li>
+              <li class="cat-item cat-item-71"><a href="http://www.aliued.com/category/life/" title="life">团队生活</a> </li>
+              <li class="cat-item cat-item-58"><a href="http://www.aliued.com/category/user-research/" title="查看用户研究下的所有文章">用户研究</a> </li>
+              <li class="cat-item cat-item-1"><a href="http://www.aliued.com/category/share/" title="查看知识分享下的所有文章">知识分享</a> </li>
+              <li class="cat-item cat-item-15"><a href="http://www.aliued.com/category/visual-design/" title="查看视觉设计下的所有文章">视觉设计</a> </li>
+            </ul>
+          </li>
+          <li id="recent-posts-2" class="widget widget_recent_entries">   <h2 class="widgettitle">近期文章</h2></li>
+          <li id="linkcat-18" class="widget widget_links"><h2 class="widgettitle">友情链接</h2>
+            <ul class='xoxo blogroll'>
+              <li><a href="http://www.aliued.cn" title="阿里巴巴中文站UED">Alibaba(中文站)UED</a></li>
+              <li><a href="http://ued.alipay.com" title="支付宝UED">Alipay UED</a></li>
+              <li><a href="http://mux.baidu.com" title="百度MUX团队博客">Baidu MUX</a></li>
+              <li><a href="http://vc.changyou.com/index.shtml">Changyou Visual Center</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+ 
+
+    <!-- footer -->
+    <div class="layout-footer content-wrap">
+      <p>
+        <a href="http://www.aliued.com/about-us/">团队介绍</a> | <a href="http://www.aliued.com/category/share/">团队分享</a> | <a href="http://www.aliued.com/join-us/">加入我们</a>
+      </p>
+      <p>
+        Copyright &copy; 1998 - 2012  阿里巴巴国际站UED . All Rights Reserved. <a href="http://www.miitbeian.gov.cn/" target="_blank">浙ICP备09002987号-30</a> <script src="http://s24.cnzz.com/stat.php?id=4284718&web_id=4284718" language="JavaScript"></script>
+      </p>
+    </div>
+  </div>
+</body>
+</html>
